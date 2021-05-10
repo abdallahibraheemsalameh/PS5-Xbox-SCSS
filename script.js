2278348bb9ece2d1dbbf7011e293e26d0dfc4845
abdallah <abdallahsalameh43@yahoo.com>
2021-05-10 06:26:44 +0300

    first upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// const ps5 = document.querySelector(".ps5");
+// const xbox = document.querySelector(".xbox");
+
+// ps5.addEventListener("click", function () {
+//   ps5.classList.add("active-l");
+//   xbox.classList.add("active-l");
+// });
